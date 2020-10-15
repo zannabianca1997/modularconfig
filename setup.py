9d28264eb1dff2039002108c26110b9c8ab618fa
zannabianca1997 <zannabianca199712@gmail.com>
2020-10-15 18:40:30 +0200

    setted up more reasonable version and developement status

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-version = '1.0'
+version = '0.2'
 
 setup(
     name='configloader',
@@ -15,7 +15,7 @@ setup(
     author_email='zannabianca199712@gmail.com',
     license='BSD',
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 3',
